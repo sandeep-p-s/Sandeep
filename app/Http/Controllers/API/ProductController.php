b86f24c30bd4c9407bcfd2bef7e049c26d68605e
sandeep090456 <sandeepps2468@gmail.com>
2023-06-07 11:57:46 +0530

    changes

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+
+    return response()->json(['products' => $products], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'category' => 'required|string',
+        ]);
+
+        $imagePath = $request->file('image')->store('products', 'public');
+
+        $product = new Product();
+        $product->name = $request->input('name');
+        $product->image = $imagePath;
+        $product->category = $request->input('category');
+        $product->save();
+
+        return response()->json(['message' => 'Product uploaded successfully'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'admin',
+            'email'=>'admin@admin.com',
+            'password'=>bcrypt('admin'),
+        ]);
+        User::create([
+            'name'=>'sandeep',
+            'email'=>'sandeep@gmail.com',
+            'password'=>bcrypt('password'),
+        ]);
+    }
+}
